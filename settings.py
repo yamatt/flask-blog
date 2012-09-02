df6aaf950ca2af82e9c95eb73d1cfd6121615b9a
Matthew Copperwaite <mattcopp@gmail.com>
2012-09-02 16:12:29 +0100

    timedelta doesn't accept years. Changed it to days

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ SUBTITLE="An awesome blog."
 SECRET_KEY = """CHANGE ME!!!"""
 ADMIN_USERNAME = "admin"
 PER_PAGE=15
-PERMANENT_LOGON_TIMEOUT = {'year': 1}
+PERMANENT_LOGON_TIMEOUT = {'days': 365}
 
 #Database
 DATABASE_TYPE="sqlite"
