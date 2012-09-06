3765e439203d48771693efb6cd37b8498ce7c311
Matthew Copperwaite <mattcopp@gmail.com>
2012-09-06 22:04:50 +0100

    Fleshed out usage and concreted design

--- models/base.py
+++ models/base.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from enums import AuthorisationLevels
 import settings
 import bcrypt
@@ -7,48 +6,81 @@ class DataBaseError(Exception):
     pass
 
 class DataBase(object):
-
     def __init__(self, connection_string):
         """
-        Set up all your database server connection here.
-        """
-        raise NotImplementedError()
-        
-    def get_post(self, year=None, month=None, post_name=None):
-        """
-        Return a complete Post object that matches the parameters.
-        @param year: an integer year value for the year the post was published.
+        This is called when the database is connected to. The connection
+        string parameter is the string configured in settings.py and
+        should be used to access the database.
+        :param connection_string:a string used to find and access the
+         database.
         """
         raise NotImplementedError()
         
     def get_post_by_id(self, id_val):
         """
-        Return a post from the id.
+        Used to retreive a post from an id associated with that post in
+        your database. This is most useful when trying to access
+        non-published items.
+        :param id_val:any value that uniquely identifies a post
+        Returns a Post object
         """
         raise NotImplementedError()
         
-    def get_posts(self, year=None, month=None, page=None):
+    def get_all_posts(self):
         """
-        Return a list of Post objects that match the parameters.
+        A catch-all used to return every post in the database. Usually
+        used to find non-published posts.
+        Returns an iterable full of Post objects.
         """
         raise NotImplementedError()
-        
-    def get_published_posts(self, page=None):
+
+    def get_published_post(self, year, month, post_name):
         """
-        Return array of items for front page
+        Returns a post that has been published. Used most often for
+        individual blog posts being viewed.
+        :param year:a year integer for that post.
+        :param month:a month integer for that post.
+        :param post_name:the unique name for that year/month combo.
+        Returns Post object.
+        """
+        raise NotImplementedError()
+    
+    def get_published_posts(self, year=None, month=None, page=None):
+        """
+        Returns a list of posts that have been published. Used most
+        often for the front page, but also archives.
+        The year and month param are optional to limit results from just
+        the archives if required. With neither specified it returns the
+        front page results.
+        :param year:a year integer for that post.
+        :param month:a month integer for that post.
+        :param post_name:the unique name for that year/month combo.
+        Returns Post object.
         """
         raise NotImplementedError()
         
     def add_post(self, post):
+        """
+        Takes a fully formatted Post object and stores it in the
+        database.
+        :param post:a Post object to save.
+        """
         raise NotImplementedError()
-        
+
     def get_user(self, user):
+        """
+        Takes a username and returns a User object populated from the
+        database entry.
+        :param user:username used to uniquely identify user.
+        """
         raise NotImplementedError()
         
     def add_user(self, user):
-        raise NotImplementedError()
-        
-    def update_user(self, user):
+        """
+        Takes a User object and stores it in the database.
+        It will have to check it's unique constraints.
+        :param user:User object to store.
+        """
         raise NotImplementedError()
         
         
@@ -58,9 +90,9 @@ class Item(object):
     """
     def __init__(self, content, user, updated=None):
         """
-        @param content: the content of your entry.
-        @param user: the user object this item was created by.
-        @param updated: a datetime object of when this entry was last updated.
+        :param content:the content of your entry.
+        :param user:the user object this item was created by.
+        :param updated:a datetime object of when this entry was last updated.
         """
         self.content = content
         self.user = user
@@ -72,7 +104,7 @@ class Comment(Item):
     """
     def __init__(self, content, user, post, updated=None):
         """
-        @param post: a post object that this comment is the child of.
+        :param post:a post object that this comment is the child of.
         """
         self.post = post
         super(Comment, self).__init__(content, user, created)
@@ -87,10 +119,17 @@ class Page(Item):
     
 class Post(Page):
     """
-    A post object that represents a new post.
+    Represents a blog entry.
+    
+    It is recommended you extend this object through inheritance so that
+    you can more easily manipulate your database.
     """
     @classmethod
     def from_form(cls, form, user):
+        """
+        Accepts the WTForms Form object and a User object to turn in to
+        a Post.
+        """
         id_val = form.id_val.data
         title = form.title.data
         content = form.content.data
@@ -107,6 +146,17 @@ class Post(Page):
         
         
 class User(object):
+    """
+    Represents a user in the database.
+    
+    Be aware that it is required that you hash a users password before
+    entering it in to the database. This is for very good security
+    reasons in-case you ever had your database stolen. This technique
+    will prevent the attackers from easily brute-forcing the passwords.
+    
+    It is recommended you extend this object through inheritance so that
+    you can more easily manipulate your database.
+    """
     @staticmethod
     def hash_password(password):
         return bcrypt.hashpw(password, bcrypt.gensalt())
